e3a5bb275eaf9228186840ba36b07992a9d20e0f
TohaRodiv <toha.rodiv@yandex.ru>
2021-12-25 01:40:49 +1000

    Миграции

--- src/config/DatabaseConfig.ts
+++ src/config/DatabaseConfig.ts
@@ -13,11 +13,13 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const DatabaseConfig: TypeOrmModuleOptions = {
 	type: "postgres",
-	host: process.env.DB_HOST || "localhost",
-	port: +process.env.DB_PORT || 3306,
-	username: process.env.DB_USER || "root",
-	password: process.env.DB_PASSWORD || "password",
-	database: process.env.DB_NAME || "electronly",
+	host: process.env.DB_HOST,
+	port: +process.env.DB_PORT,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	synchronize: false,
+	migrationsRun: true,
 	entities: [
 		User,
 		Article,
@@ -30,17 +32,12 @@ export const DatabaseConfig: TypeOrmModuleOptions = {
 		// ProductImage,
 		File,
 	],
-	synchronize: false,
 	migrations: [
-		"src/migration/**/*.ts"
-	],
-	subscribers: [
-		"src/subscriber/**/*.ts"
+		"src/_db/migrations/**/*.ts"
 	],
 	cli: {
 		entitiesDir: "src/",
-		migrationsDir: "src/migration",
-		subscribersDir: "src/subscriber"
+		migrationsDir: "src/_db/migrations",
 	}
 };
 
