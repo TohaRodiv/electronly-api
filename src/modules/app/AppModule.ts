bfe3ca371ac1a6fdf7b29e3091d897ae176a41ab
TohaRodiv <toha.rodiv@yandex.ru>
2022-01-25 23:38:06 +1000

    Удаление тестовых сервисов

--- src/modules/app/AppModule.ts
+++ src/modules/app/AppModule.ts
@@ -9,8 +9,6 @@ import { FileModule } from "#modules/file/FileModule";
 import { AppService } from "./AppService";
 import { SocialModule } from "#modules/social/SocialModule";
 import { NotificationModule } from "#modules/notification/NotificationModule";
-import { NotificationService } from "#modules/notification/NotificationService";
-import { TelegramService } from "#modules/social/telegram/TelegramService";
 
 @Module({
 	imports: [
